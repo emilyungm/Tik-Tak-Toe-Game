e65d4d04356eeb4fb62e6dd15f8130824577b958
emilyungm <107110433+emilyungm@users.noreply.github.com>
2024-02-29 19:24:05 +1100

    Completed the game with full functioning logic + UI

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Tik-Tak-Toe-Game
 
-Simple tik-tak-toe game implemented using pygame
+Simple tik-tak-toe game for two players implemented using pygame
+
+Play by clicking down the grid square you wish to place your token in!
--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import pygame 
 from pygame.locals import *
+import time
 
 
 current_player = 'X'
@@ -15,6 +16,7 @@ SCREEN_HEIGHT = GAME_HEIGHT + 150
 WHITE = (255, 255, 255)
 PINK = (250, 87, 212)
 AQUA = (68, 227, 211)
+GREEN = (75, 209, 113)
 GREY = (237, 235, 235)
 
 LINE_WIDTH = 5
@@ -39,43 +41,57 @@ def draw_game_grid():
     # font for msgs
     calibri_font = pygame.font.SysFont('calibri', 30)
 
+    # display msg to screen
+    user_msg = calibri_font.render("It's Player " + current_player + "'s turn!", False, (0, 0, 0))
+    text_box = user_msg.get_rect(center=(SCREEN_WIDTH/2, 50))
+    screen.blit(user_msg, text_box)
+
+    # white box
+    pygame.draw.rect(screen, WHITE, pygame.Rect(25, 100, 300, 300))
+
+    # horizontal lines
+    pygame.draw.line(screen, PINK, (25, 100), (325, 100), LINE_WIDTH)
+    pygame.draw.line(screen, PINK, (25, 200), (325, 200), LINE_WIDTH)
+    pygame.draw.line(screen, PINK, (25, 300), (325, 300), LINE_WIDTH)
+    pygame.draw.line(screen, PINK, (25, 400), (325, 400), LINE_WIDTH)
+
+    # vertical lines
+    pygame.draw.line(screen, PINK, (25, 100), (25, 400), LINE_WIDTH)
+    pygame.draw.line(screen, PINK, (125, 100), (125, 400), LINE_WIDTH)
+    pygame.draw.line(screen, PINK, (225, 100), (225, 400), LINE_WIDTH)
+    pygame.draw.line(screen, PINK, (325, 100), (325, 400), LINE_WIDTH)
+
+    for row in range(len(game_board)):
+        for col in range(len(game_board)):
+            if game_board[row][col] == 'X':
+                pygame.draw.line(screen, AQUA, (col*100 + 40, row*100 + 115), (col*100 + 40 + 73, row*100 + 115 + 73), LINE_WIDTH)
+                pygame.draw.line(screen, AQUA, (col*100 + 40 + 73, row*100 + 115), (col*100 + 40, row*100 + 115 + 73), LINE_WIDTH)
+            elif game_board[row][col] == 'O':      
+                pygame.draw.circle(screen, GREEN, (col*100+40+36,  row*100+115+36), 38, LINE_WIDTH)
+                
     # if someone has won, display win message
     if winner:
+        # cover up user msg so text can be replaced
+        pygame.draw.rect(screen, GREY, (0, 0, SCREEN_WIDTH, 75))
         user_msg = calibri_font.render("Player " + winner + " won!", False, (0, 0, 0))
         text_box = user_msg.get_rect(center=(SCREEN_WIDTH/2, 50))
         screen.blit(user_msg, text_box)
 
     elif round == 9 and winner is None:
+        # cover up user msg so text can be replaced
+        pygame.draw.rect(screen, GREY, (0, 0, SCREEN_WIDTH, 75))
         user_msg = calibri_font.render("There was a draw :/", False, (0, 0, 0))
         text_box = user_msg.get_rect(center=(SCREEN_WIDTH/2, 50))
-        screen.blit(user_msg, text_box)
-    
-    else:
-
-        # display msg to screen
-        user_msg = calibri_font.render("It is Player " + current_player + "'s turn!", False, (0, 0, 0))
-        text_box = user_msg.get_rect(center=(SCREEN_WIDTH/2, 50))
-        screen.blit(user_msg, text_box)
-
-        # white box
-        pygame.draw.rect(screen, WHITE, pygame.Rect(25, 100, 300, 300))
-
-        # horizontal lines
-        pygame.draw.line(screen, PINK, (25, 100), (325, 100), LINE_WIDTH)
-        pygame.draw.line(screen, PINK, (25, 200), (325, 200), LINE_WIDTH)
-        pygame.draw.line(screen, PINK, (25, 300), (325, 300), LINE_WIDTH)
-        pygame.draw.line(screen, PINK, (25, 400), (325, 400), LINE_WIDTH)
-
-        # vertical lines
-        pygame.draw.line(screen, PINK, (25, 100), (25, 400), LINE_WIDTH)
-        pygame.draw.line(screen, PINK, (125, 100), (125, 400), LINE_WIDTH)
-        pygame.draw.line(screen, PINK, (225, 100), (225, 400), LINE_WIDTH)
-        pygame.draw.line(screen, PINK, (325, 100), (325, 400), LINE_WIDTH)
+        screen.blit(user_msg, text_box)     
 
     # update the UI
     pygame.display.update()
 
-    # cover up user msg so text can be replaces
+    if round == 9 or winner:
+        time.sleep(3)
+        pygame.quit()
+
+    # cover up user msg so text can be replaced
     pygame.draw.rect(screen, GREY, (0, 0, SCREEN_WIDTH, 75))
 
 
@@ -83,7 +99,6 @@ def user_interaction_logic():
 
     if event.type == pygame.MOUSEBUTTONDOWN:
         x, y = pygame.mouse.get_pos()
-        # print(x,y)
 
         if 110 <= y <= 190:         # row 0
             if 35 <= x <= 115:      # row 0, column 0
@@ -110,22 +125,17 @@ def user_interaction_logic():
                 write_player_input_to_grid(2, 2)
 
 
-    # game_winning_logic()
-
 def write_player_input_to_grid(row_num: int, col_num: int):
     global round, current_player
-    print(round)
-    if not game_board[row_num][col_num]:
+    if not game_board[row_num][col_num]:            # if the square isn't occupied, let player take it
         game_board[row_num][col_num] = current_player
-        # print(game_board)
         round += 1
 
         if round >= 5:      # game can only be won after 5 turns
             check_winner()
+
         switch_curr_player()
-    # else:
-    #     print("wrong")
-    # #     user_interaction_logic()
+
         
 def switch_curr_player():
     global current_player
@@ -149,8 +159,10 @@ def check_winner():
         
         if x_count == 3:
             winner = 'X'
+            return
         elif o_count == 3:
             winner = 'O'
+            return
         
     # check for vertical win
         for row in range(len(game_board)):
@@ -165,25 +177,28 @@ def check_winner():
             
             if x_count == 3:
                 winner = 'X'
+                return
             elif o_count == 3:
                 winner = 'O'
+                return
             
     # check for diagonal win (left to right)
     if game_board[0][0] == game_board[1][1] == game_board[2][2]:
         if game_board[0][0] == 'X':
             winner = 'X'
+            return
         if game_board[0][0] == 'O':
             winner = 'O'
+            return
 
     # check for diagonal win (right to left)
     if game_board[0][2] == game_board[1][1] == game_board[2][0]:
         if game_board[0][2] == 'X':
             winner = 'X'
+            return
         if game_board[0][2] == 'O':
             winner = 'O'
-
-    return False
-
+            return
 
 # game loop
 game_running = True
@@ -197,7 +212,3 @@ while game_running:
         else:
             draw_game_grid()
             user_interaction_logic()
-
-    # pygame.display.update()
-
-pygame.quit()
